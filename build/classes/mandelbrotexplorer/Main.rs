638ca4f98445604e0c0d927803a47e9bda8a0a12
Sean <seanbeecroft@gmail.com>
2018-08-09 10:18:38 -0400

    Moved old repo to github

---
+++ build/classes/mandelbrotexplorer/Main.rs
@@ -0,0 +1,7 @@
+mandelbrotexplorer.Position
+mandelbrotexplorer.MandelbrotFrame$1
+mandelbrotexplorer.MandelbrotPanel
+mandelbrotexplorer.MandelbrotFrame$2
+mandelbrotexplorer.MandelbrotFrame
+mandelbrotexplorer.MandelbrotFrame$3
+mandelbrotexplorer.Main
